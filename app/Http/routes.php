09d446a4f664ff9d930e479110a3e4a4f0ed981e
Gustavo Ramirez <gustavo.ramirez@ingeniandoweb.com>
2016-07-14 22:27:00 -0400

    Authenticate Your Users

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,14 @@
 |
 */
 
+Route::get('/',function(){
+    return view('welcome');
+});
+Route::auth();
+Route::get('/home', 'HomeController@index');
 Route::get('cards','CardsController@index');
 Route::get('cards/{card}','CardsController@show');
 Route::post('cards/{card}/notes','NotesController@store');
 Route::get('notes/{note}/edit','NotesController@edit');
 Route::patch('notes/{note}','NotesController@update');
+
